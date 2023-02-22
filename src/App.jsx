02f2f0d0771ea1429500c93b82c97b18b29ab6ab
Babatunde Joseph <babatundejoseph85@gmail.com>
2023-02-22 15:58:00 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen  flex justify-center items-center space-x-4 mx-auto py-4">
+      <h1 className="text-black font-thin text-2xl underline underline-offset-8 hover:text-blue-500 hover:cursor-pointer">Hello world, this is react hooks tutorial</h1>
+    </div>
+  )
+}
+
+export default App
