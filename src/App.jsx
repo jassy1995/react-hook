96ce4265848a5dfbf332658f792abe8c0497980d
Babatunde Joseph <babatundejoseph85@gmail.com>
2023-02-23 12:11:18 +0100

    added the useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { useState, useRef, useEffect } from "react";
+
 function App() {
+  const [name, setName] = useState("");
+  const renderCount = useRef(1);
+  const inputRef = useRef();
+  const prevValue = useRef('');
+
+  useEffect(() => {
+    renderCount.current = renderCount.current + 1
+  })
+  useEffect(() => {
+    inputRef.current.focus();
+  })
+  useEffect(() => {
+    prevValue.current = name;
+  })
+  const focus = () => {
+    inputRef.current.focus();
+  }
+
   return (
     <div className="h-screen  flex justify-center items-center space-x-4 mx-auto py-4">
-      <h1 className="text-black font-thin text-2xl underline underline-offset-8 hover:text-blue-500 hover:cursor-pointer">Hello world, this is react hooks tutorial</h1>
+      <div className="flex flex-col space-y-4">
+        <input type="text" ref={inputRef} value={name} onChange={(e) => setName(e.target.value)} className="bg-white p-2 ring-1 ring-blue-300 outline-none border-none text-black rounded-md text-xl" />
+        <span className="text-black text-xl">My name is {name} and my previous name is {prevValue.current}</span>
+        <span className="text-black text-xl">I render {renderCount.current} </span>
+        <button className="border border-slate-300 bg-slate-200 text-black py-1 rounded-md" onClick={focus}>focus</button>
+      </div>
     </div>
   )
 }
