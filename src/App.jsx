431be21f4aff0db799a3136a11559a69b84b57da
Babatunde Joseph <babatundejoseph85@gmail.com>
2023-02-25 15:06:33 +0100

    useeffectLayOut add

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { useEffect, useLayoutEffect, useState } from "react"
 function App() {
+  const [greeting, setGreeting] = useState("Hi")
+
+  useEffect(() => {
+    setGreeting("This is the home page")
+    console.log(greeting)
+  }, [])
+
+  useLayoutEffect(() => {
+    setGreeting("Hello, Mr Babatunde")
+    console.log(greeting)
+  }, [])
+
   return (
     <div className="h-screen  flex justify-center items-center space-x-4 mx-auto py-4">
-      <h1 className="text-black font-thin text-2xl underline underline-offset-8 hover:text-blue-500 hover:cursor-pointer">Hello world, this is react hooks tutorial</h1>
+      <h1 className="text-black font-thin text-2xl underline underline-offset-8 hover:text-blue-500 hover:cursor-pointer">{greeting}</h1>
     </div>
   )
 }
